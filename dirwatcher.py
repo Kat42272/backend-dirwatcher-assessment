c86c0122d10ea9364cec6fa48634c8a98c8aa109
Kathryn Anderson <kat42272@yahoo.com>
2020-07-17 19:11:13 -0700

    Fixed errors

--- dirwatcher.py
+++ dirwatcher.py
@@ -19,6 +19,8 @@ import logging
 import time
 import os
 import signal
+from os import walk
+
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:\
@@ -28,28 +30,7 @@ logit = logging.getLogger(__name__)
 
 dict_of_files = {}
 
-
-def magic_text_func(n):
-    """
-    This function uses specific "magic" text to look through the
-    specified directory for the "magic" text
-    """
-    
-    try:
-        for key in dict_of_files:
-            with open(n.dir[0] + "/" + key, "r") as f:
-                lines = f.readlines()
-                for i, line in enumerate(lines):
-                    result = re.search('(.+)' + n.magic[0] + '(.+)', line)
-                    if result and i not in dict_of_files[key]:
-                        dict_of_files[key].append(i)
-                        logit.info(f"Magic text found in file {key} on line\{str(i + 1)}")
-    except Exception as inst:
-        logit.exception(f"The following error has occured {inst}")
-
-
-
-def spec_file_func(n):
+def spec_file_func(ns):
     """
     This function will search the specific directory for files that
     match pre-assigned text.
@@ -58,27 +39,27 @@ def spec_file_func(n):
     temp_spec_file_func = {}
 
     try:
-        if os.path.isdir(n.dir[0]):
-            logging.info(f"Directory {n.dir[0]} is being searched")
-            for f in n.dir[0]:
+        if os.path.isdir(ns.dir[0]):
+            logging.info(f"Directory {ns.dir[0]} is being searched")
+            for f in walk(ns.dir[0]):
                 file_list = f[2]
             for file in file_list:
-                if n.ext is not None:
-                    if file[-len(n.ext[0]):] == n.ext[0]:
+                if ns.ext is not None:
+                    if file[-len(ns.ext[0]):] == ns.ext[0]:
                         temp_spec_file_func.setdefault(file, [])
                 else:
                     temp_spec_file_func.setdefault(file, [])
         else:
-            logit.info(f"{n.dir[0]} directory does not exist ")
+            logit.info(f"The {ns.dir[0]} directory does not exist ")
     except AttributeError:
         pass
     except Exception as inst:
         logit.exception(f"The {inst} error has taken place.")
     
-    match_file_func(temp_spec_file_func, n)
+    match_file_func(temp_spec_file_func, ns)
 
 
-def match_file_func(temp_spec_file, n):
+def match_file_func(temp_spec_file, ns):
     """
     This function looks for files that have changed from the original
     directory and compares it to a temp directory to recognize changes.
@@ -91,13 +72,34 @@ def match_file_func(temp_spec_file, n):
                 dict_of_files[k] = []
         for k in dict_of_files:
             if k not in temp_spec_file:
-                logit.info(f"The Direcotry has removed file {k}")
+                logit.info(f"The Directory has removed file {k}")
                 dict_of_files.pop(k, None)
     except RuntimeError:
         pass
     except Exception as inst:
-        logit.exception(f"The following error has occured {inst}")
-    magic_text_func(n)
+        logit.exception(f"The {inst} error has occured.")
+    magic_text_func(ns)
+
+
+
+def magic_text_func(ns):
+    """
+    This function uses specific "magic" text to look through the
+    specified directory for the "magic" text
+    """
+    
+    try:
+        for key in dict_of_files:
+            with open(ns.dir[0] + "/" + key, "r") as f:
+                lines = f.readlines()
+                for i, line in enumerate(lines):
+                    result = re.search('(.+)' + n.magic[0] + '(.+)', line)
+                    if result and i not in dict_of_files[key]:
+                        dict_of_files[key].append(i)
+                        logit.info(
+                            f"Magic text found in file: {key} was found on line\{str(i + 1)}")
+    except Exception as inst:
+        logit.exception(f"The {inst} error has occured.")
 
 
 time_to_start = time.time()
@@ -110,7 +112,8 @@ def run_time_func():
     """
 
     return time.time() - time_to_start
-    
+
+
 def create_parser():
     """
     Command Line Parser
@@ -128,7 +131,8 @@ def create_parser():
 
 exit_flag = False
 
-def sig_func():
+
+def sig_func(sig_num, frame):
     """
     Program responds to SIGINT and SIGTERM signals from the OS. Signal
     events are logged so that a human can determine what the signal was.
@@ -151,19 +155,19 @@ def main(args):
                  {56 * '-'}\n")
     signal.signal(signal.SIGINT, sig_func)
     signal.signal(signal.SIGTERM, sig_func)
-    logger = 1
+    logger_int = 1
     while not exit_flag:
         try:
             parser = create_parser()
-            n = parser.parse_args(args)
-            print(n)
-            if n.int != 1:
-                logger = int(n.int[0])
-            spec_file_func(n)
+            ns = parser.parse_args(args)
+            print(ns)
+            if ns.int != 1:
+                logger_int = int(ns.int[0])
+            spec_file_func(ns)
         except Exception as inst:
-            logit.exception(f"The following error has occured {inst}")
+            logit.exception(f"The {inst} error has occured.")
             spec_file_func([])
-        time.sleep(logger)
+        time.sleep(logger_int)
 
 
 if __name__ == '__main__':
